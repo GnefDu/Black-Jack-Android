58dc2d49cd32b1bb1002f48c698c95a988b574f8
GnefDu <fengdu88@gmail.com>
2020-07-26 00:04:10 -0400

    Variables declared and initalized, onclicklisters set and tested

--- app/src/main/java/com/example/blackjack/MainActivity.java
+++ app/src/main/java/com/example/blackjack/MainActivity.java
@@ -41,8 +41,78 @@ public class MainActivity extends AppCompatActivity {
 
         //Text Views
 
+        text_display = (TextView) findViewById(R.id.display);
 
+        text_player = (TextView) findViewById(R.id.player);
+
+        text_dealer = (TextView) findViewById(R.id.dealer);
+
+
+        // ImageViews
+
+        player0 = (ImageView) findViewById(R.id.player0);
+
+        player1 = (ImageView) findViewById(R.id.player1);
+
+        player2 = (ImageView) findViewById(R.id.player2);
+
+        player3 = (ImageView) findViewById(R.id.player3);
+
+        player4 = (ImageView) findViewById(R.id.player4);
+
+        player5 = (ImageView) findViewById(R.id.player5);
+
+        player6 = (ImageView) findViewById(R.id.player6);
+
+        player7 = (ImageView) findViewById(R.id.player7);
+
+        player8 = (ImageView) findViewById(R.id.player8);
+
+        player9 = (ImageView) findViewById(R.id.player9);
+
+        dealer0 = (ImageView) findViewById(R.id.dealer0);
+
+        dealer1 = (ImageView) findViewById(R.id.dealer1);
+
+        dealer2 = (ImageView) findViewById(R.id.dealer2);
+
+        dealer3 = (ImageView) findViewById(R.id.dealer3);
+
+        dealer4 = (ImageView) findViewById(R.id.dealer4);
+
+        dealer5 = (ImageView) findViewById(R.id.dealer5);
+
+        dealer6 = (ImageView) findViewById(R.id.dealer6);
+
+        dealer7 = (ImageView) findViewById(R.id.dealer7);
+
+        dealer8 = (ImageView) findViewById(R.id.dealer8);
+
+        dealer9 = (ImageView) findViewById(R.id.dealer9);
+
+        //OnClickListener
+        button_hit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                player0.setImageResource(R.drawable.ad);
+            }
+        });
+
+        button_stand.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                player0.setImageResource(R.drawable.ah);
+            }
+        });
+
+        button_reset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                player0.setImageResource(R.drawable.as);
+            }
+        });
+
+        }
 
 
     }
-}
