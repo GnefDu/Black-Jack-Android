ef69d1ad618332bcadc2d1fd1ade4a4aa29d08a5
GnefDu <fengdu88@gmail.com>
2020-07-26 19:55:26 -0400

    Game methods done

--- app/src/main/java/com/example/blackjack/Game.java
+++ app/src/main/java/com/example/blackjack/Game.java
@@ -7,11 +7,16 @@ public class Game {
     public ArrayList<Integer> deck;
     public ArrayList<Integer> player;
     public ArrayList<Integer> dealer;
+    public boolean playerBust;
+    public boolean dealerBust;
+
     //constructor
     public Game(){
         this.deck = new ArrayList<Integer>();
         this.player = new ArrayList<Integer>();
         this.dealer = new ArrayList<Integer>();
+        this.playerBust = false;
+        this.dealerBust = false;
     }
     //create deck 0-51
     public void createDeck(){
@@ -26,7 +31,7 @@ public class Game {
         for (int i = 0; i < this.deck.size(); i++) {
             int randPos = rand.nextInt(this.deck.size());
             int temp = this.deck.get(i);
-            this.deck.set(i,this.deck.get(randPos));
+            this.deck.set(i, this.deck.get(randPos));
             this.deck.set(randPos, temp);
         }
     }
@@ -45,6 +50,116 @@ public class Game {
         this.deck.remove(randPos);
     }
 
+    public int countPlayer(){
+
+        boolean ace = false;
+        int count = 0;
+
+        for (int i = 0; i < this.getPlayer().size(); i++){
+            if(this.getPlayer().get(i) < 4){
+                ace = true;
+            }
+        }
+        if(!ace){
+            for (int i = 0; i < this.getPlayer().size(); i++){
+                if(this.getPlayer().get(i) > 38){
+                    count += 10;
+                }
+                else{
+                    count += (this.getPlayer().get(i) / 4) + 1;
+                }
+            }
+        }
+        else{
+            for (int i = 0; i < this.getPlayer().size(); i++){
+                if(this.getPlayer().get(i) < 4){
+                    count += 11;
+                }
+                else if(this.getPlayer().get(i) > 38){
+                    count += 10;
+                }
+                else{
+                    count += (this.getPlayer().get(i) / 4) + 1;
+                }
+            }
+        }
+        if(count > 21){
+            count = 0;
+            for (int i = 0; i < this.getPlayer().size(); i++){
+                if(this.getPlayer().get(i) > 38){
+                    count += 10;
+                }
+                else{
+                    count += (this.getPlayer().get(i) / 4) + 1;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int countDealer(){
+
+        boolean ace = false;
+        int count = 0;
+
+        for (int i = 0; i < this.getDealer().size(); i++){
+            if(this.getDealer().get(i) < 4){
+                ace = true;
+            }
+        }
+        if(!ace){
+            for (int i = 0; i < this.getDealer().size(); i++){
+                if(this.getDealer().get(i) > 38){
+                    count += 10;
+                }
+                else{
+                    count += (this.getDealer().get(i) / 4) + 1;
+                }
+            }
+        }
+        else{
+            for (int i = 0; i < this.getDealer().size(); i++){
+                if(this.getDealer().get(i) < 4){
+                    count += 11;
+                }
+                else if(this.getDealer().get(i) > 38){
+                    count += 10;
+                }
+                else{
+                    count += (this.getDealer().get(i) / 4) + 1;
+                }
+            }
+        }
+        if(count > 21){
+            count = 0;
+            for (int i = 0; i < this.getDealer().size(); i++){
+                if(this.getDealer().get(i) > 38){
+                    count += 10;
+                }
+                else{
+                    count += (this.getDealer().get(i) / 4) + 1;
+                }
+            }
+        }
+        return count;
+    }
+
+    public boolean playerBust(){
+        int count = countPlayer();
+        if(count > 21){
+            playerBust = true;
+        }
+        return playerBust;
+    }
+
+    public boolean dealerBust(){
+        int count = countDealer();
+        if(count > 21){
+            dealerBust = true;
+        }
+        return dealerBust;
+    }
+
 
     //Getters
     public ArrayList<Integer> getDeck(){
@@ -57,6 +172,13 @@ public class Game {
 
     public ArrayList<Integer> getPlayer(){
         return player;
+    }
+
+    public boolean getPlayerBust(){
+        return playerBust;
+    }
+    public boolean getDealerBust(){
+        return dealerBust;
     }
 
 
